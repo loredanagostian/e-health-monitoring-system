0a515062279937fe8aa3d3cabc367ad273a9fc67
Lore Gostian <loredana.gostian@cobaltsign.com>
2025-04-22 23:10:32 +0300

    added Lato font, assets, project structure, onboarding screen, custom button, helpers and constants

---
+++ e_health_monitoring_system_frontend/lib/helpers/strings_helper.dart
@@ -0,0 +1,6 @@
+class StringsHelper {
+  static const String onboardingTitle = "Your e-Health App";
+  static const String onboardingMessage =
+      "Digital healthcare platform - secure patient records, online appointment booking, e-prescriptions, and treatment monitoring";
+  static const String getStarted = "Get started";
+}
