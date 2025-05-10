bb218a16b0b36d18484a41e5d47315161cb9267e
Vlad Tomuta <vlad.tomuta@student.upt.ro>
2025-05-10 14:48:43 +0300

    Connected backend with frontend for the appointment screen so that it will load the doctors from the database instead of mock doctors

--- e_health_monitoring_system_frontend/lib/widgets/doctor_card.dart
+++ e_health_monitoring_system_frontend/lib/widgets/doctor_card.dart
@@ -42,7 +42,20 @@ class DoctorCard extends StatelessWidget {
                       topLeft: Radius.circular(15),
                       bottomLeft: Radius.circular(15),
                     ),
-                    child: Image.asset(doctorPhotoPath, fit: BoxFit.contain),
+                    child: Image.network(
+                      doctorPhotoPath,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) => Image.asset(
+                        'assets/images/mockup_doctor.png',
+                        fit: BoxFit.cover,
+                      ),
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        return Center(child: CircularProgressIndicator());
+                      },
+                    ),
+
+
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(
